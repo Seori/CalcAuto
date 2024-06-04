c8c73295736afe0a439b6e576727a6927a5f4a18
Tony <cyssori@gmail.com>
2024-06-04 14:43:06 +0900

    초기 커밋.

---
+++ CalcAuto/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace CalcAuto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbplusrate = new System.Windows.Forms.TextBox();
+            this.tbminusrate = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblminusprice = new System.Windows.Forms.Label();
+            this.lblplusprice = new System.Windows.Forms.Label();
+            this.lblprice = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "입력금액";
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(85, 14);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(191, 21);
+            this.tbPrice.TabIndex = 1;
+            this.tbPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbPrice.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPrice_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "매도비율(+)";
+            // 
+            // tbplusrate
+            // 
+            this.tbplusrate.Location = new System.Drawing.Point(85, 45);
+            this.tbplusrate.Name = "tbplusrate";
+            this.tbplusrate.Size = new System.Drawing.Size(49, 21);
+            this.tbplusrate.TabIndex = 3;
+            // 
+            // tbminusrate
+            // 
+            this.tbminusrate.Location = new System.Drawing.Point(231, 45);
+            this.tbminusrate.Name = "tbminusrate";
+            this.tbminusrate.Size = new System.Drawing.Size(49, 21);
+            this.tbminusrate.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(158, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "매도비율(-)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(135, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(281, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(279, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "원";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblminusprice);
+            this.groupBox1.Controls.Add(this.lblplusprice);
+            this.groupBox1.Controls.Add(this.lblprice);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(14, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(282, 111);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "계산결과";
+            // 
+            // lblminusprice
+            // 
+            this.lblminusprice.AutoSize = true;
+            this.lblminusprice.Location = new System.Drawing.Point(108, 85);
+            this.lblminusprice.Name = "lblminusprice";
+            this.lblminusprice.Size = new System.Drawing.Size(27, 12);
+            this.lblminusprice.TabIndex = 20;
+            this.lblminusprice.Text = "0 원";
+            // 
+            // lblplusprice
+            // 
+            this.lblplusprice.AutoSize = true;
+            this.lblplusprice.Location = new System.Drawing.Point(108, 60);
+            this.lblplusprice.Name = "lblplusprice";
+            this.lblplusprice.Size = new System.Drawing.Size(27, 12);
+            this.lblplusprice.TabIndex = 19;
+            this.lblplusprice.Text = "0 원";
+            // 
+            // lblprice
+            // 
+            this.lblprice.AutoSize = true;
+            this.lblprice.Location = new System.Drawing.Point(108, 34);
+            this.lblprice.Name = "lblprice";
+            this.lblprice.Size = new System.Drawing.Size(27, 12);
+            this.lblprice.TabIndex = 18;
+            this.lblprice.Text = "0 원";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(20, 34);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 12);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "기준금액 : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 85);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 12);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "매도금액(-) : ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "매도금액(+) : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 28);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "계산하기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 229);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbminusrate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbplusrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "CalcAuto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbplusrate;
+        private System.Windows.Forms.TextBox tbminusrate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblminusprice;
+        private System.Windows.Forms.Label lblplusprice;
+        private System.Windows.Forms.Label lblprice;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CalcAuto/Form1.cs
@@ -0,0 +1,73 @@
+﻿using MineFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcAuto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fnCalc();
+        }
+
+        public void fnCalc()
+        {
+            if (string.IsNullOrEmpty(tbPrice.Text))
+            {
+                MessageBox.Show("기준금액을 입력하세요.");
+                tbPrice.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tbplusrate.Text))
+            {
+                MessageBox.Show("매도(+)금액을 입력하세요.");
+                tbplusrate.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tbminusrate.Text))
+            {
+                MessageBox.Show("매도(-)금액을 입력하세요.");
+                tbminusrate.Focus();
+                return;
+            }
+
+            float f_price = float.Parse(tbPrice.Text);
+            double plus_price = Math.Round(f_price * ((100 + float.Parse(tbplusrate.Text)) / 100));
+            double minus_price = Math.Round(f_price * ((100 - float.Parse(tbminusrate.Text)) / 100));
+
+            lblprice.Text = StringCls.AmountComma(f_price.ToString()) + " 원";
+            lblplusprice.Text = StringCls.AmountComma(plus_price.ToString()) + " 원";
+            lblminusprice.Text = StringCls.AmountComma(minus_price.ToString()) + " 원";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbplusrate.Text = "1.0";
+            tbminusrate.Text = "3.0";
+        }
+
+        private void tbPrice_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                tbPrice.Text = StringCls.AmountComma(tbPrice.Text);
+                fnCalc();
+            }
+        }
+    }
+}
